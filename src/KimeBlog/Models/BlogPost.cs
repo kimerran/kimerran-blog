65113105734efa3e00e1440248d75010ecebc915
kimerran <mh.neri@gmail.com>
2014-05-20 10:59:46 +0800

    added ToTitleCase

--- src/KimeBlog/Models/BlogPost.cs
+++ src/KimeBlog/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,21 @@ namespace KimeBlog.Models
     public class BlogPost
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+                this._title = textInfo.ToTitleCase(value);
+            }
+        }
+
         public string Url { get; set; }
         public string Author { get; set; }
         public string PubDate { get; set; }
@@ -18,8 +33,8 @@ namespace KimeBlog.Models
         public string FacebookUsername { get; set; }
         public string TwitterUsername { get; set; }
 
-        public  List<String> Tags {get;set;}
-        public bool  CanPrev { get; set; }
+        public List<String> Tags { get; set; }
+        public bool CanPrev { get; set; }
         public bool CanNext { get; set; }
 
         public int NextId { get; set; }
